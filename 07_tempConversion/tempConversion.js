77afac48222fee2056142cafa48ac230c9052ca1
Vicktorious23 <90080625+VICKTORIOUS23@users.noreply.github.com>
2024-11-10 04:58:21 -0600

    Completion of Node 07 Temp Conversion
    Utilizing .toFixed and Number call
    to achieve goals.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  let para1 = (((faren - 32) * 5) / 9);
+  let para2 = para1.toFixed(1);
+  return Number(para2);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let para3 = (((cels * 9) / 5) + 32);
+  let para4 = para3.toFixed(1);
+  return Number(para4);
 };
 
 // Do not edit below this line
