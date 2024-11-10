7fb884d87b216088193b9c9b0fe00b6a3977a9a6
Vicktorious23 <90080625+VICKTORIOUS23@users.noreply.github.com>
2024-11-10 04:06:02 -0600

    Completion of Node 05 sumAll
    ** Need to revisit parsing ! values.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(para1, para2) {
+  let sum = 0;
+  let bigInt=0;
+  let smallInt=0;
+
+  if (para1 < 0 || para2 < 0) {return "ERROR"}
+    else if (para1 < para2) {smallInt = para1, bigInt = para2}
+    else {smallInt = para2, bigInt = para1};
+  if (!Number.isInteger(smallInt) || !Number.isInteger(bigInt)) {return "ERROR";} 
+
+    for (let i = smallInt; i <= bigInt; i++) {
+        sum += i;
+    }
+    return sum;
+
+
 
 };
 
