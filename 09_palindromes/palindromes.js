855dfbd36f62a517039edf05569b83cf570943a6
Vicktorious23 <90080625+VICKTORIOUS23@users.noreply.github.com>
2024-11-10 17:50:51 -0600

    Completed Node 09 Palindromes
    Usages of .split .join .reverse
    many attempts failed at excluding punctuation
    including only alphabet and numbers was much
    much simpler.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    const numLet = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    b = a.toLowerCase();
+    c = b.split('').filter((character) => numLet.includes(character)).join('');
+    rev = c.split('').reverse().join('');
+
+    if (rev === c) {return true;}
+    else {return false;}
+    
+
 
 };
 
