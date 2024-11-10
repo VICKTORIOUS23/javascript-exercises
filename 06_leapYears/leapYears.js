a43d79270d8bb8a2f586a188727f9cbff390f7cb
Vicktorious23 <90080625+VICKTORIOUS23@users.noreply.github.com>
2024-11-10 04:37:31 -0600

    Completion of Node 06 leapYears.
    Attempted to use very complicated math
    only to find a really easy solution.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(para1) {
+    byFour = para1 % 4 === 0;
+    byCentury = para1 % 100 === 0;
+    byFourCent = para1 % 400 === 0;
+    
+    if (byFour && (!byCentury || byFourCent)) {
+        return true;
+    } else {
+        return false;
+    }
+
 
 };
 
