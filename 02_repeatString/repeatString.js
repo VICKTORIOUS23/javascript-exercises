3c1fbea6cb62e351027bc8c31fd7abd064b981c4
Vicktorious23 <90080625+VICKTORIOUS23@users.noreply.github.com>
2024-11-10 03:18:29 -0600

    01 and 02 Completed
    Completed Intro Node 01 - Hello World
    Completed Node 02 - Repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(para1, para2) {
+    let foo = "";
+    if (para2 < 0) {
+        return "ERROR";}
+    else {    
+    for (let i = 1; i <= para2; i++) {
+        foo = foo + para1;
+    }
+    
+    return foo;
 };
+}
 
 // Do not edit below this line
 module.exports = repeatString;
