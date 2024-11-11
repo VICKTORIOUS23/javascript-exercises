257c8c5a97851086b5d0ab08f81442a0b7e7f963
Vicktorious23 <90080625+VICKTORIOUS23@users.noreply.github.com>
2024-11-10 18:04:22 -0600

    Completion of node 10 Fibonacci
    Heavily, heavily relied upon research
    for this node. This is a node that I'm
    setting to the side to revisit multiple
    times to fully understand.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
+    if (count == 0) return 0;
+    if (count < 0) return "OOPS";
 
-};
+
+    const fib = [0, 1];
+     for (let i = 2; i <= count; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+     }
+     return fib[count];
+    }
 
 // Do not edit below this line
 module.exports = fibonacci;
